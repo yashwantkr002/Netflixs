139236005da336da62e923a691fc0e677cc01263
yashwantkr002 <60185821+yashwantkr002@users.noreply.github.com>
2024-06-23 13:34:30 +0530

    Update settings.py

--- NetAppp/settings.py
+++ NetAppp/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sonarai002@gmail.com'
-EMAIL_HOST_PASSWORD = 'vvlnjhvgcjmjgpkn'
+EMAIL_HOST_PASSWORD = 'yourpass'
 EMAIL_USE_TLS = True
